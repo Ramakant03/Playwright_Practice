b98bc0655f4031e6dfc8822def313f78448e169c
Ramakant Gupta <ramakantgupta1573@gmail.com>
2026-05-22 23:40:27 +0530

    headless false

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    headless: true,
     screenshot: 'only-on-failure',
     video: 'on',
     viewport: null, // disables default 1280x720
